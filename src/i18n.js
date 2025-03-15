ae12a6431d34a19b200217c356ba30b7bec1b3be
ahsan200112 <ahsan200112@gmail.com>
2025-03-16 00:14:20 +0500

    default ar lang

--- src/i18n.js
+++ src/i18n.js
@@ -6,8 +6,8 @@ i18n.use(initReactI18next).init({
     en: { translation: require('./locales/en.json') },
     ar: { translation: require('./locales/ar.json') }
   },
-  lng: 'en', // Default language
-  fallbackLng: 'en',
+  lng: 'ar', // Default language
+  fallbackLng: 'ar',
   interpolation: { escapeValue: false } // React escape handling
 });
 
