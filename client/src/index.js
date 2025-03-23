f7b3ee0d6116e72caace736873e2c2f4241200af
ahsan200112 <ahsan200112@gmail.com>
2025-03-23 05:27:55 +0500

    minor changes

--- client/src/index.js
+++ client/src/index.js
@@ -8,13 +8,11 @@ import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </HelmetProvider>
-  </React.StrictMode>
+  <HelmetProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </HelmetProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
